39b8a4cd5a4fa3ec17ec1b649d7574e87a50c4c8
varenya <varenya@Varenyas-MacBook-Pro.local>
2017-02-01 23:10:44 +0530

    DB relations and security checks implementations

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,6 +2,9 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
+  plug Discuss.Plugs.RequireAuth when action in [:new,:create,:edit,:update,:delete]
+  plug :check_topic_owner when action in [:edit,:update,:delete]
+
   def index(conn,_params) do
       topics = Repo.all(Topic)
       render conn,"index.html",topics: topics
@@ -13,7 +16,9 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn,%{ "topic" => topic}) do
-    changeset = Topic.changeset(%Topic{} , topic)
+    changeset = conn.assigns.user
+                  |> build_assoc(:topics)
+                  |> Topic.changeset(topic)
     case Repo.insert(changeset) do
       {:ok , _topic} ->
         conn
@@ -48,5 +53,17 @@ defmodule Discuss.TopicController do
         conn
         |> put_flash(:info,"Topic Deleted!")
         |> redirect(to: topic_path(conn,:index))
+  end
+
+  def check_topic_owner(conn,_params) do
+      %{ params: %{ "id" => topic_id } } = conn
+      if Repo.get(Topic,topic_id).user_id == conn.assigns.user.id do
+        conn
+      else
+        conn
+        |> put_flash(:error,"You cannot edit that")
+        |> redirect(to: topic_path(conn,:index))
+        |> halt()
+      end
   end
 end
